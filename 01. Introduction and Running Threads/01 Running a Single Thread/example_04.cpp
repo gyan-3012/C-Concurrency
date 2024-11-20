cfcf5364390231afbe4a7f859b791be947fa6158
GYAN <gyanprakashsingh065@gmail.com>
2024-11-20 23:53:39 +0530

    Randomness of thread events

---
+++ 01. Introduction and Running Threads/01 Running a Single Thread/example_04.cpp
@@ -0,0 +1,38 @@
+// Randomness of events
+
+#include <iostream>
+#include <thread>
+
+void threadFunction()
+{
+  std::this_thread::sleep_for(std::chrono::milliseconds(50)); // simulate work
+  std::cout  << "Finished work 1 in thread\n";
+
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(50)); // simulate work
+  std::cout  << "Finished work 2 in thread\n";
+}
+
+int main()
+{
+  // create thread
+  std::thread t(threadFunction);
+
+  // do something in main()
+  std::this_thread::sleep_for(std::chrono::milliseconds(50)); //simulate work
+  std::cout << "Finished work 1 in main\n";
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  std::cout << "Finished work 2 in main\n";
+
+  // wait for thread to finish
+  t.join();
+
+  return 0;
+}
+
+// Output: The output order may varry in different execution
+// Finished work 1 in main
+// Finished work 1 in thread
+// Finished work 2 in main
+// Finished work 2 in thread
