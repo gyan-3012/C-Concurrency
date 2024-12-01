9be144e974f38492f0768f233962bd8f88021bc1
GYAN <gyanprakashsingh065@gmail.com>
2024-12-01 10:48:07 +0530

    Promise - Future One time use communication channel

--- 02. Passing Data Between Threads/01 Promises and Futures/example_01.cpp
+++ 02. Passing Data Between Threads/01 Promises and Futures/example_01.cpp
@@ -22,6 +22,7 @@ int main()
   std::future<std::string> ftr = prms.get_future();
 
   // start thread and pass promise as argument
+  // promise object cann't be copied so we are moving it as rvalue reference
   std::thread t(modifyMessage, std::move(prms), messageToThread);
 
   // print original message to console
@@ -36,3 +37,11 @@ int main()
 
   return 0;
 }
+
+// Imp. Points about get() method on future comm. end
+// - get will wait until data is set on the promise end using set_value() method
+// - if get() is called later after set_value() has been called then it will get the value without any delay
+
+// Output:
+// Original message from main(): My Message
+// Modified message from thread(): My Message has been modified
